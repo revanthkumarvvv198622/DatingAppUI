495d5909798e51b7978f458127bf4a782fd8043d
revanthkumarvvv198622 <revanthkumarvvv@gmail.com>
2026-02-18 15:15:32 -0500

    Changes in Index page by adding 3rd party css

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,25 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+
+  private httpClient = inject(HttpClient);
+  protected readonly title = 'Dating App - Angular';
+  protected members = signal<any>([]);
+  protected url = 'https://localhost:5001/api/members/getmembers';
+
+    async ngOnInit() {
+      this.members.set(await this.LoadMembers());
+    }
+
+    async LoadMembers() {
+       try{
+            return await lastValueFrom(this.httpClient.get(this.url));
+       }
+        catch(error){
+          console.log(error);
+          throw error;
+        }
+    }
+
 }
